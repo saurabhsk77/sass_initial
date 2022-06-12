1e82c0d9970fe4bf19ec3a1913e639f14cef3ca4
Saurabh Kumar <saurabh_kumar1@intuit.com>
2022-06-12 11:20:24 +0530

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/App.scss";
+import styled from "styled-components";
 
+const Title = styled.h1`
+  color: red;
+  text-align: center;
+`;
+
+const Wrapper = styled.section`
+  padding: 2px;
+  background: yellow;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Wrapper>
+        <Title>Hello everyone</Title>
+        <p>hey</p>
+      </Wrapper>
     </div>
   );
 }
